07657eaf42dd887f6201f768f46767c724c0c125
RichardSyfer <deadwalker87@gmail.com>
2015-08-02 18:23:49 +0300

    geekbrains-les1-hw

--- www/index.php
+++ www/index.php
@@ -7,5 +7,24 @@
 <body>
     <h1>Hello world</h1>
     <p><?php echo 2+3; ?></p>
+    <p>
+        <?php
+            $a = 10;
+            $b = 15;
+
+            echo "Start value <br>";
+            echo "a= ". $a ."<br>";
+            echo "b= ". $b ."<br>";
+
+            $a ^= $b;
+            $b ^= $a;
+            $a ^= $b;
+
+            echo "After:<br>";
+
+            echo "a=". $a ."<br>";
+            echo "b= ". $b ."<br>";
+        ?>
+    </p>
 </body>
 </html>
